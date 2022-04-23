ce9c525069b3cb40ff37434ab85624845e5efc37
mookpnr <phatnaree.sukp@bumail.net>
2022-04-23 15:16:44 +0700

    class/13/firebase-installation

--- src/context/FirebaseConfig.js
+++ src/context/FirebaseConfig.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/database';
 const firebaseConfig = {
-  apiKey: "AIzaSyDOPJ9_X7MbrMXHkai922aLK2NVa7xbGjo",
-  authDomain: "gi472-pwa-course-2022.firebaseapp.com",
-  projectId: "gi472-pwa-course-2022",
-  storageBucket: "gi472-pwa-course-2022.appspot.com",
-  messagingSenderId: "967644458652",
-  appId: "1:967644458652:web:6f71ad83773786006b8759",
-  measurementId: "G-Z7Z74R200B",
-  databaseURL: "https://gi472-pwa-course-2022-default-rtdb.asia-southeast1.firebasedatabase.app"
+  apiKey: "AIzaSyArs1UZulK84V2KDIgO9votTClpcOTvQls",
+  authDomain: "class13-4136.firebaseapp.com",
+  projectId: "class13-4136",
+  storageBucket: "class13-4136.appspot.com",
+  messagingSenderId: "407917544132",
+  appId: "1:407917544132:web:21d4be3528aebd10b2bf91",
+  measurementId: "G-DTLD3VEPL5",
+  databaseURL: "https://class13-4136-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
 
 firebase.initializeApp(firebaseConfig);
